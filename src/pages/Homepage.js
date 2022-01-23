563c113df7efb6c9982536350f612384be1e7948
windsuzu <windsuzu@gmail.com>
2022-01-23 16:14:07 +0800

    [add] fetch pexels api

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Picture from "../components/Picture";
 import Search from "../components/Search";
+import * as api from "../api/pexels";
 
 const Homepage = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(
+        () =>
+            api
+                .fetchPexelsData(api.curated_url())
+                .then((res) => res.json())
+                .then((data) => setData(data.photos)),
+        []
+    );
+
     return (
         <>
             <Search />
+            <div className="pictures">
+                {data && data.map((d) => <Picture data={d} />)}
+            </div>
         </>
     );
 };
