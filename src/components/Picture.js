fff1e5d1a5c55f720a3982817477c95099746e90
windsuzu <windsuzu@gmail.com>
2022-01-24 23:33:39 +0800

    [add] gallery css, rwd

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -3,11 +3,20 @@ import React from "react";
 const Picture = ({ data }) => {
     return (
         <div className="picture">
-            <p>{data.photographer}</p>
-            <img src={data.src.medium} alt={data.alt} title={data.alt} />
-            <a target="_blank" rel="noreferrer" href={data.src.large}>
+            {/* <p>{data.photographer}</p> */}
+            <div className="imgContainer">
+                <a
+                    href={data.src.xlarge || data.src.large}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <div className="hint">Download</div>
+                    <img src={data.src.large} alt={data.alt} title={data.alt} />
+                </a>
+            </div>
+            {/* <a target="_blank" rel="noreferrer" href={data.src.large}>
                 Download Image
-            </a>
+            </a> */}
         </div>
     );
 };
